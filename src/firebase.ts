ef43fbf611f502b08daea571163af3e14238b885
Carlos Garcia-Marquez <carlos@lkxz.net>
2021-02-21 18:31:16 +0100

    Deployed to new server

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 export const firebaseApp = firebase.initializeApp({
-  projectId: 'd0-n0t35',
-  appId: '1:82564438436:web:80d31d24c6ede8e219c931',
-  databaseURL: 'https://d0-n0t35.firebaseio.com',
-  storageBucket: 'd0-n0t35.appspot.com',
+  projectId: 'do-not-130',
+  appId: '1:965453400551:web:74f934fd1e07350f8d5542',
+  databaseURL: 'https://do-not-130.firebaseio.com',
+  storageBucket: 'do-not-130.appspot.com',
   locationId: 'europe-west',
-  apiKey: 'AIzaSyAyCPlm6aLJh8yoOLxLH1Og1THEyuJ7yIo',
-  authDomain: 'd0-n0t35.firebaseapp.com',
-  messagingSenderId: '82564438436'
+  apiKey: 'AIzaSyBzp4lq7T-q57VA186OgKRDEAQFyPaSixA',
+  authDomain: 'do-not-130.firebaseapp.com',
+  messagingSenderId: '965453400551'
 });
